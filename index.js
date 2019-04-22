6a1928fc72b82e5c529eb6eb0b4b34d3a3190364
Brad Ayers <brad@bradayers.com>
2019-04-22 23:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(customers) {
 function currentLine(line) {
   var lineOrder = "The line is currently: ";
   for (var place = 0; place < line.length; place++) {
-    lineOrder = lineOrder + (place + 1) + `. ${line[place]}` + (place < line.length ? `, ` : ``);
+    lineOrder = lineOrder + (place + 1) + `. ${line[place]}` + (place < line.length-1 ? `, ` : ``);
   }
   return (line.length > 0 ? lineOrder : "The line is currently empty.");
 }
